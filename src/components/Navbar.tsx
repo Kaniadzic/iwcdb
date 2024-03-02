5489fcbfd26a64ec8fe8754459bd31bb9b2b5ac6
Kaniadzic <szymon.kania@edu.uekat.pl>
2024-03-02 22:21:43 +0100

    View: navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+
 export const Navbar = () => {
     return (
         <div className="flex">
-            navbar
+            <Link to="/" className="link-main"> Library </Link>
+            <Link to="/deckbuilder" className="link-main"> Deck Builder </Link>
+            <Link to="/glossary" className="link-main"> Glossary </Link>
+            <Link to="/about" className="link-main"> About </Link>
         </div>
     );
 };
