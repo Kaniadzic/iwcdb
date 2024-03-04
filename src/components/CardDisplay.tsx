d5a9f812c65793c4505b2bd81f2f528a5ab2eb86
Kaniadzic <szymon.kania@edu.uekat.pl>
2024-03-04 18:58:23 +0100

    Card display: proper details displaying

---
+++ src/components/CardDisplay.tsx
@@ -0,0 +1,52 @@
+import { CardDisplayProps } from "../interfaces/CardDisplayProps";
+
+/**
+ * 
+ * @param subTypesArray 
+ * @returns 
+ */
+const formatSubTypes = (subTypesArray: string[] | null | undefined): string => {
+    let subtypes = "";
+
+    subTypesArray?.forEach((subtype) => {
+        subtypes += `${subtype} `;
+    });
+
+    return subtypes;
+}
+
+export const CardDisplay = (props: CardDisplayProps) => {
+  return (
+    <div className="flex card-display-container">
+      <div className="flex column card-display-left">
+        <img src={props.cardData?.imageUrl} />
+      </div>
+
+      <div className="flex column card-display-right">
+        <div className="flex card-display-top">
+            <button className="button-close-display" onClick={() => {props.clickFunction()}}>✕</button>
+        </div>
+        <h2>{props.cardData?.name}</h2>
+        <h4>
+          {props.cardData?.superType} {props.cardData?.type} -{" "}
+          {formatSubTypes(props.cardData?.subTypes)}
+        </h4>
+        <p>— Rarity: {props.cardData?.rarity}</p>
+        <p>— Purity: </p>
+        <p>— Resource cost: {props.cardData?.resourceCost} </p>
+
+        {/* Attack, health morale cost and attributes are displayed only for Character cards */}
+        {props.cardData?.type == "Character" && (
+          <>
+            <p>— Health: {props.cardData?.health} </p>
+            <p>— Attack: {props.cardData?.attack} </p>
+            <p>— Morale cost: {props.cardData.moraleCost} </p>
+            <p>— Attributes: {props.cardData.attributes?.toString()} </p>
+          </>
+        )}
+
+        <p>— Card: {props.cardData?.setNumber} of {props.cardData?.setName} set </p>
+      </div>
+    </div>
+  );
+};
--- src/pages/Library.tsx
+++ src/pages/Library.tsx
@@ -4,6 +4,7 @@ import { Card as ICard } from "../interfaces/Card";
 import { useEffect, useState } from "react";
 import { cardsRef } from "../config/References";
 import "../styles/Card.css";
+import { CardDisplay } from "../components/CardDisplay";
 
 export const Library = () => {
   const [cardsList, setCardsList] = useState<ICard[]>([]);
@@ -41,6 +42,10 @@ export const Library = () => {
     setIsCardClicked(true);
   };
 
+  const handleCloseDisplayClick = () => {
+    setIsCardClicked(false);
+  };
+
   return (
     <div className="flex container-library">
       {cardsList.map((card) => {
@@ -49,33 +54,7 @@ export const Library = () => {
 
       {isCardClicked && (
         <div className="flex background-blur">
-          <div className="flex card-display-container">
-            <div className="flex column card-display-left">
-              <img src={clickedCardData?.imageUrl} />
-            </div>
-
-            <div className="flex column">
-              <div className="flex column card-display-right">
-                <p>{clickedCardData?.name}</p>
-                <p>— Rarity: {clickedCardData?.rarity}</p>
-                <p>— Purity: </p>
-                <p>— Health: {clickedCardData?.health} </p>
-                <p>— Attack: {clickedCardData?.attack} </p>
-                <p>— </p>
-                <p>— </p>
-                <p>— </p>
-              </div>
-              <div className="flex column card-display-bottom">
-                <p
-                  onClick={() => {
-                    setIsCardClicked(false);
-                  }}
-                >
-                  Click to close
-                </p>
-              </div>
-            </div>
-          </div>
+          <CardDisplay cardData={clickedCardData} clickFunction={handleCloseDisplayClick} />
         </div>
       )}
     </div>
